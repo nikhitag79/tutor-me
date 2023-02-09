31abe2830a4556c3ab55998f554f28c4fd669767
Joshua Garrison <jgarrison@reallaer.com>
2023-02-09 18:41:59 -0500

    changing directories

--- testproject1/asgi.py
+++ testproject1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for testproject1 project.
+ASGI config for something project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproject1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'something.settings')
 
 application = get_asgi_application()
