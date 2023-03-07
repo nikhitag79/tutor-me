7d91c06dc9e84c7642c48655b368ee975e81a8c4
aidant03 <aidan.tan03@gmail.com>
2023-03-07 14:25:50 -0500

    working on user type select

--- testproject1/settings.py
+++ testproject1/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # BASE_DIR / 'staticfiles'
 
 # The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
