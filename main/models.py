7f9b26eb1974ce15b4a7d892b1db637e803d87be
evaterry4 <bzt4em@virginia.edu>
2023-03-26 10:49:19 -0400

    started to add hourly rate to tutors

--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 from django.forms import widgets
 from oauth_app.models import User
 from django.contrib.auth.models import Group
 import requests
 import json
 from oauth_app.models import User
+
 
 class View(models.Model):
     def get_json_data(url):
@@ -20,7 +22,7 @@ class View(models.Model):
 
 # I have used a quick database set up for this. We can edit later, it just helps to show
 class ClassList(models.Model):
-    class_id = models.CharField(max_length=10, default = 'cl id')
+    class_id = models.CharField(max_length=10, default='cl id')
     class_name = models.CharField(max_length=100, default='cl name')
     # professors = models.CharField(max_length=100, default = 'cl pr')
     available_tutors = models.BooleanField(default=False)
@@ -28,27 +30,25 @@ class ClassList(models.Model):
     def __str__(self):
         return self.class_id
 
+
 # The items are the tutors
 class Item(models.Model):
     classlist = models.ForeignKey(ClassList, on_delete=models.CASCADE)
     tutor_first_name = models.CharField(max_length=30, default='first')
     tutor_last_name = models.CharField(max_length=30, default='last')
+    tutor_rate = MoneyField(max_digits=2, decimal_places=2, default_currency='USD', default=0.00)
 
     def __str__(self):
         return self.tutor_first_name + ' ' + self.tutor_last_name
 
 
-
-
 class ClassDatabase(models.Model):
-    class_id = models.CharField(max_length=10, default = 'no class id')
-    class_mnen = models.CharField(max_length=100, default = 'no class mnemonic')
+    class_id = models.CharField(max_length=10, default='no class id')
+    class_mnen = models.CharField(max_length=100, default='no class mnemonic')
     class_name = models.CharField(max_length=100, default='no class name')
-    professors = models.CharField(max_length=100, default = 'no professor')
+    professors = models.CharField(max_length=100, default='no professor')
     available_tutors = models.BooleanField(default=False)
     tutors = models.ForeignKey(Group, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
-        return self.class_id + ' ' + self.class_name + ' ' +  self.professors
-
-
+        return self.class_id + ' ' + self.class_name + ' ' + self.professors
