270e6e41496836e8bddf1c4dfb1826a1e350a6a5
evaterry4 <bzt4em@virginia.edu>
2023-04-04 14:26:38 -0400

    testing default user type and default rate

--- main/tests.py
+++ main/tests.py
@@ -2,6 +2,10 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 from main.models import ClassDatabase
+from oauth_app.models import User
+import djmoney
+from djmoney.money import Money
+
 
 
 # Create your tests here.
@@ -11,6 +15,32 @@ class TestClass(TestCase):
         response=c.get('/student_home/')
         self.assertEqual(response.status_code, 200)
 
+    def test_defaultUserType(self):
+        self.tutor = User.objects.create()
+        self.assertEqual(self.tutor.user_type, 2)
+
+    def test_declaringTutorType(self):
+        self.tutor = User.objects.create(user_type=1)
+        self.assertEqual(self.tutor.user_type, 1)
+
+    def test_defaultTutorRate(self):
+        self.tutor = User.objects.create(user_type=1)
+        self.assertEqual(self.tutor.tutor_rate, djmoney.money.Money('0.0', 'USD'))
+
+    def test_changingTutorRate(self):
+        self.tutor = User.objects.create(user_type=1)
+        self.tutor.tutor_rate = djmoney.money.Money('10.00', 'USD')
+        self.assertEqual(self.tutor.tutor_rate, djmoney.money.Money('10.00', 'USD'))
+
+
+    # Tutor = 1
+    # Student = 2
+    # Administration = 3
+    # has_selected_role = models.BooleanField(default=False)
+    # available_classes = {(Tutor, 'Tutor'), (Student, 'Student'), (Administration, 'Administration')}
+    # user_type = models.PositiveIntegerField(choices=available_classes, default=2)
+    # tutor_rate = MoneyField(max_digits=4, decimal_places=2, default_currency='USD', default=0.00)
+
     # def test_my_class_db_values(self):
     #     self.cd = ClassDatabase.objects.create(class_id="CS3240", class_mnen="CS",
     #                                            class_name="Software Dev", professors="Horton")
@@ -18,6 +48,7 @@ class TestClass(TestCase):
     #     self.assertEqual(self.cd.class_mnen, "CS")
     #     self.assertEqual(self.cd.class_name, "Software Dev")
     #     self.assertEqual(self.cd.professors, "Horton")
+
     #
     # def test_course_filter(self):
     #     ClassDatabase.objects.create(class_id="CS3240", class_mnen="CS",
