7d2d15cb4c88240e3c377629c46a71b47dc02629
pnavsingh <pnavsingh@gmail.com>
2023-04-13 18:19:29 -0400

    Test case for update

--- main/tests.py
+++ main/tests.py
@@ -1,8 +1,9 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
 from django.utils import timezone
 
 from main.models import ClassDatabase, Event
+from main.views import update
 from oauth_app.models import User
 import djmoney
 from djmoney.money import Money
@@ -148,6 +149,37 @@ class TestClass(TestCase):
         data= response.json()
         print(response.json())
         self.assertEqual(len(data), 0)
+    
+
+    def test_update(self):
+         self.factory = RequestFactory()
+         self.group = Group.objects.create(name='Test Group', id=9)
+         self.tutor = User.objects.create_user(user_type=1, username="tutor_name", id=9, password='testpass')
+         self.group.user_set.add(self.tutor)
+         self.group.save()
+
+         self.assertTrue(self.client.login(username='tutor_name', password='testpass'))
+
+         self.event = Event.objects.create(id=9,name="CS3240 Tutoring 1",tutor=self.tutor,start=timezone.now(),end=timezone.now() + timezone.timedelta(hours=2))
+
+         request = self.factory.get(
+            '/update/',
+            {
+                'id': 9,
+                'title': 'CS3240 New Title',
+            }
+        )
+         #request.user = self.tutor
+
+        # Call the update function
+         response = update(request)
+
+        # Check if the JsonResponse is returned
+         #self.assertIsInstance(response, JsonResponse)
+
+        # Check if the event data was updated correctly
+         updated_event = Event.objects.get(id=9)
+         self.assertEqual(updated_event.name, 'CS3240 New Title')
         
         # just starting this, thinking we can test inputting a previous
         # date and making sure it gets deleted & vice versa
